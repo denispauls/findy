9d0c482e4b5bf5e8d5311ed9973c081bf073c16e
denispauls <denis.pauls@smail.th-koeln.de>
2023-06-12 10:52:25 +0200

    Link to Maps etc

---
+++ app/src/main/java/com/example/findy/presentation/maps/MapScreen.kt
@@ -0,0 +1,140 @@
+package com.example.findy.presentation.maps
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.findy.R
+import com.example.findy.navigation.Screens
+import com.example.findy.ui.theme.LocationUtils
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.maps.android.compose.CameraPositionState
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+
+
+
+@SuppressLint("MissingPermission")
+@Composable
+fun MapScreen(fusedLocationProviderClient: FusedLocationProviderClient,
+              navController: NavController) {
+
+    var currentLocation by remember { mutableStateOf(LocationUtils.getDefaultLocation()) }
+
+    val cameraPositionState = rememberCameraPositionState()
+    cameraPositionState.position = CameraPosition.fromLatLngZoom(
+        LocationUtils.getPosition(currentLocation), 12f
+    )
+
+    var requestLocationUpdate by remember { mutableStateOf(true) }
+
+    MyGoogleMap(
+        currentLocation = currentLocation,
+        cameraPositionState = cameraPositionState,
+        onGpsIconClick = {
+            requestLocationUpdate = true
+        },
+        onFriendsButtonClick = {
+            navController.navigate(Screens.FriendsScreen.route)
+        }
+    )
+
+    if (requestLocationUpdate) {
+        LocationPermissionsAndSettingDialogs(
+            updateCurrentLocation = {
+                requestLocationUpdate = false
+                LocationUtils.requestLocationResultCallback(fusedLocationProviderClient) { locationResult ->
+                    locationResult.lastLocation?.let { location ->
+                        currentLocation = location
+                    }
+                }
+            }
+        )
+    }
+}
+
+@Composable
+private fun MyGoogleMap(
+    currentLocation: Location?,
+    cameraPositionState: CameraPositionState,
+    onGpsIconClick: () -> Unit,
+    onFriendsButtonClick: () -> Unit
+) {
+    val mapUiSettings by remember {
+        mutableStateOf(
+            MapUiSettings(zoomControlsEnabled = false)
+        )
+    }
+
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState,
+        uiSettings = mapUiSettings,
+    ) {
+        currentLocation?.let { location ->
+            Marker(
+                state = MarkerState(position = LocationUtils.getPosition(location)),
+                title = "Aktuelle Position"
+            )
+        }
+    }
+
+    GpsIconButton(
+        onIconClick = onGpsIconClick,
+        onFriendsButtonClick = onFriendsButtonClick
+    )
+}
+
+@Composable
+private fun GpsIconButton(
+    onIconClick: () -> Unit,
+    onFriendsButtonClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Bottom,
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ) {
+            IconButton(onClick = onIconClick) {
+                Icon(
+                    modifier = Modifier.padding(bottom = 100.dp, end = 20.dp),
+                    painter = painterResource(id = R.drawable.ic_gps_fixed),
+                    contentDescription = null
+                )
+            }
+            IconButton(onClick = onFriendsButtonClick) {
+                Icon(
+                    modifier = Modifier.padding(bottom = 0.dp, end = 20.dp),
+                    painter = painterResource(id = R.drawable.ic_back_arrow),
+                    contentDescription = null
+                )
+            }
+        }
+    }
+}
